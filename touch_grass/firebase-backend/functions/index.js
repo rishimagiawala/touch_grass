94eac8508577155445b6faf198bcdf4514c578d1
Manav Patel <92553242+manavpatelUGA@users.noreply.github.com>
2023-10-14 17:06:34 -0400

    firebase stuff

--- touch_grass/firebase-backend/functions/index.js
+++ touch_grass/firebase-backend/functions/index.js
@@ -1,27 +1,20 @@
-
-//  Import function triggers from their respective submodules:
-
-  const {onCall} = require("firebase-functions/v2/https");
-  const {onDocumentWritten} = require("firebase-functions/v2/firestore");
-  const {onDocumentCreated} = require("firebase-functions/v2/firestore");
-
-//  * See a full list of supported triggers at https://firebase.google.com/docs/functions
- 
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-
-initializeApp();
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloworld = onRequest((request, response) => {
+exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
-
 
